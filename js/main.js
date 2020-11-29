2d145eb35d98a881aeb6f8aee5693e13a2f6d633
Kohei <mizu.vbct6.h2o@gmail.com>
2020-11-30 02:34:48 +0900

    20201130

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
   const close = document.getElementById('close');
+  const mnclose = document.getElementById('mn-close');
+  const cpclose = document.getElementById('cp-close');
+  const acclose = document.getElementById('ac-close');
+  const rsclose = document.getElementById('rs-close');
+
 
   open.addEventListener('click', () => {
     overlay.classList.add('show');
@@ -14,4 +19,25 @@
     overlay.classList.remove('show');
     open.classList.remove('hide');
   });
+
+  mnclose.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  cpclose.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  acclose.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  rsclose.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
 }
+
